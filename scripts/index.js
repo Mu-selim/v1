9bdeb81468169bc0b4612bb0e4046eaa55479480
selim <muhamadselim70@gmail.com>
2022-10-28 23:01:51 +0200

    technologies list

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,18 @@ socialIcons[0].insertAdjacentHTML('afterbegin', githubIcon());
 socialIcons[1].insertAdjacentHTML('afterbegin', twitterIcon());
 socialIcons[2].insertAdjacentHTML('afterbegin', linkedinIcon());
 socialIcons[3].insertAdjacentHTML('afterbegin', instagramIcon());
+
+const technologiesList = document.querySelector('.technologiesList');
+const technologies = [
+    'JavaScript (ES6+)',
+    'C#',
+    'Node.js',
+    'HTML',
+    'CSS'
+];
+technologies.forEach(technology => {
+    technologiesList.insertAdjacentHTML(
+        'beforeend',
+        `<li class="technologyItem">${technology}</li>`
+        );
+});
