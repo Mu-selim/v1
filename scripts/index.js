b6e698c1b40d60d993b7ff934ffd1ac2ef6e0d35
selim <muhamadselim70@gmail.com>
2022-10-28 18:10:14 +0200

    components rearrange

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const socialIcons = document.querySelectorAll('.socialIcons');
+
+socialIcons[0].insertAdjacentHTML('afterbegin', githubIcon());
+socialIcons[1].insertAdjacentHTML('afterbegin', twitterIcon());
+socialIcons[2].insertAdjacentHTML('afterbegin', linkedinIcon());
+socialIcons[3].insertAdjacentHTML('afterbegin', instagramIcon());
