c95a7095464d4432eb091b40e6498c9fcab73514
selim <muhamadselim70@gmail.com>
2022-10-31 00:34:23 +0200

    work section

--- scripts/lightModeToggle.js
+++ scripts/lightModeToggle.js
@@ -40,6 +40,19 @@ mode.addEventListener('click', ()=> {
         item.classList.toggle('light');
     });
     document.querySelector('.placesInner').classList.toggle('light');
+    document.querySelector('.workHeader').classList.toggle('light');
+    document.querySelectorAll('.featuredProjectTitle').forEach(item => {
+        item.classList.toggle('light');
+    });
+    document.querySelectorAll('.featuredCard').forEach(item => {
+        item.classList.toggle('light');
+    });
+    document.querySelectorAll('.toolsList').forEach(item => {
+        item.classList.toggle('light');
+    });
+
+
+
     document.querySelector('.footer').classList.toggle('light');
     document.querySelector('.footerLink').classList.toggle('light');
 });
