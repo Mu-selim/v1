ab048f1e4ed6a78e6df6b12fb5dbd621fe2d245b
selim <muhamadselim70@gmail.com>
2022-11-02 18:15:56 +0200

    theme updates

--- scripts/lightModeToggle.js
+++ scripts/lightModeToggle.js
@@ -1,6 +1,19 @@
+let theme;
+const isLight = () => {
+    let logo = document.querySelector('.topLogo');
+    if(document.querySelector('body').classList.contains('bodyLight')) {
+        theme = 'bodyLight';
+        logo.src = "images/Square-Icon-light.svg";
+    } else {
+        theme = 'bodyDark';
+        logo.src = "images/Square-Icon.svg";
+    }
+}
+isLight();
+
 document.querySelector('.mode').addEventListener('click', ()=> {
     const body = document.querySelector('body');
     body.classList.toggle('bodyLight');
-    let theme = body.classList.contains('bodyLight')? 'bodyLight': 'bodyDark';
+    isLight();
     localStorage.setItem('pageTheme', theme);
 }, false);
